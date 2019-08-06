adb88b9cc384ad05d9987022cf67d4fed09b8fbe
marciosilvestre <marciosilvestre@gmail.com>
2019-08-05 23:34:25 -0300

    d

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -48,5 +48,45 @@ namespace TodoApi.Controllers
 
         }
 
+        //POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+
+        }
+
+        //PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+                return BadRequest();
+            _context.Entry(item).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDoItem(long id)
+        {
+            var item = await _context.TodoItems.FindAsync(id);
+
+            if (item == null)
+                return NotFound();
+
+            _context.TodoItems.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+
     }
 }
